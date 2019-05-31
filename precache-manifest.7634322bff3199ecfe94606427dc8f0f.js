60e0192532463289b1fd5aa8eaa33d1589eba145
Pawel Jakubiec <pawel143700@gmail.com>
2019-05-31 23:45:22 +0200

    Updates

---
+++ precache-manifest.7634322bff3199ecfe94606427dc8f0f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24823d0ddbe40f29c1401f65dff4c0c",
+    "url": "/pollution/index.html"
+  },
+  {
+    "revision": "6c45fabca9fcd1a38224",
+    "url": "/pollution/static/css/main.b5819520.chunk.css"
+  },
+  {
+    "revision": "914e27878d5e404ddc4f",
+    "url": "/pollution/static/js/2.65f1e0d1.chunk.js"
+  },
+  {
+    "revision": "6c45fabca9fcd1a38224",
+    "url": "/pollution/static/js/main.95ee4060.chunk.js"
+  },
+  {
+    "revision": "51cd71f6e73e349bec7b",
+    "url": "/pollution/static/js/runtime~main.260eac75.js"
+  },
+  {
+    "revision": "957c4dd9aa815cc1de2040cc1259589f",
+    "url": "/pollution/static/media/github.957c4dd9.png"
+  },
+  {
+    "revision": "dc4e8593f5fae9f408f156abf1f8d1da",
+    "url": "/pollution/static/media/linkedin.dc4e8593.png"
+  },
+  {
+    "revision": "0a84d40aa3fc94610715fdfc47492b10",
+    "url": "/pollution/static/media/poll.0a84d40a.jpg"
+  }
+]);
